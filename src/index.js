49a8a561221433fb38c6fc8124fe5c00630c136f
kirillstarlight <kirillstarlight@gmail.com>
2018-11-27 19:29:55 +0300

    complete setup project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+
+import {Provider} from "react-redux";
+import store from "./store";
+
+import App from "./App";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
